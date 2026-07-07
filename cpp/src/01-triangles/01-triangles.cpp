d1289cd7ea100f232de872080c9513fbe541f477
destinyFvcker <destinyfunker@gmail.com>
2026-07-07 23:35:19 +0800

    chore: add clang-format

--- cpp/src/01-triangles/01-triangles.cpp
+++ cpp/src/01-triangles/01-triangles.cpp
@@ -11,44 +11,43 @@ enum VAO_IDs { Triangles, NumVAOs };
 enum Buffer_IDs { ArrayBuffer, NumBuffers };
 enum Attrib_IDs { vPosition = 0 };
 
-GLuint  VAOs[NumVAOs];
-GLuint  Buffers[NumBuffers];
+GLuint VAOs[NumVAOs];
+GLuint Buffers[NumBuffers];
 
-const GLuint  NumVertices = 6;
+const GLuint NumVertices = 6;
 
 //----------------------------------------------------------------------------
 //
 // init
 //
 
-void
-init( void )
+void init(void)
 {
-    glGenVertexArrays( NumVAOs, VAOs );
-    glBindVertexArray( VAOs[Triangles] );
 
-    GLfloat  vertices[NumVertices][2] = {
-        { -0.90f, -0.90f }, {  0.85f, -0.90f }, { -0.90f,  0.85f },  // Triangle 1
-        {  0.90f, -0.85f }, {  0.90f,  0.90f }, { -0.85f,  0.90f }   // Triangle 2
+    GLfloat vertices[NumVertices][2] = {
+        {-0.90f, -0.90f}, {0.85f, -0.90f}, {-0.90f, 0.85f}, // Triangle 1
+        {0.90f, -0.85f},  {0.90f, 0.90f},  {-0.85f, 0.90f}  // Triangle 2
     };
 
-    glCreateBuffers( NumBuffers, Buffers );
-    glBindBuffer( GL_ARRAY_BUFFER, Buffers[ArrayBuffer] );
-    glBufferStorage( GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0);
+    glGenVertexArrays(NumVAOs, VAOs);
+    glCreateBuffers(NumBuffers, Buffers);
+    // 下面这两行是OpenGL 4.4引入的，走的是老式OpenGL风格：先把buffer绑定到某个target，然后再对当前绑定的buffer分配storage
+    //
+    // glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+    // glBufferStorage(GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0);
+    glNamedBufferStorage(Buffers[ArrayBuffer], sizeof(vertices), vertices, 0);
 
-    ShaderInfo  shaders[] =
-    {
-        { GL_VERTEX_SHADER, "media/shaders/triangles/triangles.vert" },
-        { GL_FRAGMENT_SHADER, "media/shaders/triangles/triangles.frag" },
-        { GL_NONE, NULL }
-    };
+    ShaderInfo shaders[] = {{GL_VERTEX_SHADER, "media/shaders/triangles/triangles.vert"},
+                            {GL_FRAGMENT_SHADER, "media/shaders/triangles/triangles.frag"},
+                            {GL_NONE, NULL}};
+    GLuint program = LoadShaders(shaders);
+    glUseProgram(program);
 
-    GLuint program = LoadShaders( shaders );
-    glUseProgram( program );
+    glBindVertexArray(VAOs[Triangles]);
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
 
-    glVertexAttribPointer( vPosition, 2, GL_FLOAT,
-                           GL_FALSE, 0, BUFFER_OFFSET(0) );
-    glEnableVertexAttribArray( vPosition );
+    glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+    glEnableVertexAttribArray(vPosition);
 }
 
 //----------------------------------------------------------------------------
@@ -56,15 +55,14 @@ init( void )
 // display
 //
 
-void
-display( void )
+void display(void)
 {
-    static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+    static const float black[] = {0.0f, 0.0f, 0.0f, 0.0f};
 
     glClearBufferfv(GL_COLOR, 0, black);
 
-    glBindVertexArray( VAOs[Triangles] );
-    glDrawArrays( GL_TRIANGLES, 0, NumVertices );
+    // 这里确实也可以啥也不加 glBindVertexArray(VAOs[Triangles]);
+    glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 }
 
 //----------------------------------------------------------------------------
@@ -72,16 +70,13 @@ display( void )
 // main
 //
 
+// Windows GUI 程序使用 WinMain 作为入口，其他平台使用普通的 C/C++ main。
+// 下面的程序主体是同一份代码，只是根据平台切换入口函数签名。
 #ifdef _WIN32
-int CALLBACK WinMain(
-  _In_ HINSTANCE hInstance,
-  _In_ HINSTANCE hPrevInstance,
-  _In_ LPSTR     lpCmdLine,
-  _In_ int       nCmdShow
-)
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
+                     _In_ int nCmdShow)
 #else
-int
-main( int argc, char** argv )
+int main(int argc, char** argv)
 #endif
 {
     glfwInit();
@@ -93,8 +88,7 @@ main( int argc, char** argv )
 
     init();
 
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         display();
         glfwSwapBuffers(window);
         glfwPollEvents();
